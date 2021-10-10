e4f6e1e0db57fc24ba0913b4299950fa9f607147
MeilakhSergey <meilakh@email.com>
2021-10-10 08:55:18 +0200

    Initial commit

---
+++ src/test/java/pl/epam/meilakh/finalexam/FinalexamApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.epam.meilakh.finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
