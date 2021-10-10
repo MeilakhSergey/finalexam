8e6881cd0b7e8a4130e69579a6336fc79ebf6e52
MeilakhSergey <meilakh@email.com>
2021-10-10 11:03:04 +0200

    Deleting tests

--- src/test/java/pl/epam/meilakh/finalexam/FinalexamApplicationTests.java
+++ src/test/java/pl/epam/meilakh/finalexam/FinalexamApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FinalexamApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
