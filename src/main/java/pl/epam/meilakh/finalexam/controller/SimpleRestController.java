6e368bdccf81be1610f4ec5b8cd0d817de6c2edb
MeilakhSergey <meilakh@email.com>
2021-10-10 08:57:43 +0200

    Adding controller

---
+++ src/main/java/pl/epam/meilakh/finalexam/controller/SimpleRestController.java
@@ -0,0 +1,13 @@
+package pl.epam.meilakh.finalexam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello";
+    }
+}
