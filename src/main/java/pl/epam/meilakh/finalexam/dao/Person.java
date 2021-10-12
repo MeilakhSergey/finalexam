926828e671ae9ddbd8b04ba60528364bfa8b1326
MeilakhSergey <meilakh@email.com>
2021-10-12 15:05:35 +0200

    uncommenting

--- src/main/java/pl/epam/meilakh/finalexam/dao/Person.java
+++ src/main/java/pl/epam/meilakh/finalexam/dao/Person.java
@@ -3,17 +3,17 @@ package pl.epam.meilakh.finalexam.dao;
 import lombok.Getter;
 import lombok.Setter;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-//@Entity
-//@Getter @Setter
+@Entity
+@Getter @Setter
 public class Person {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
