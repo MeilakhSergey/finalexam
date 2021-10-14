40401409230a04039b3e394cec567f753454dbfc
MeilakhSergey <meilakh@email.com>
2021-10-14 10:31:50 +0200

    uncomment

--- src/main/java/pl/epam/meilakh/finalexam/dao/PersonRepository.java
+++ src/main/java/pl/epam/meilakh/finalexam/dao/PersonRepository.java
@@ -1,8 +1,8 @@
 package pl.epam.meilakh.finalexam.dao;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-public interface PersonRepository /*extends JpaRepository<Person, Long>*/ {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
